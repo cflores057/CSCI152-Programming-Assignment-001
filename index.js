9706290c20c2f61de58365652eaedbf163a43622
Carlos Flores <cflores057@mail.fresnostate.edu>
2023-03-16 16:09:37 -0700

    upload node files
    uploading node files

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const http = require('http'); 
+/*const http = require('http'); 
 const data = require('./source')
-//const data = {name: 'Carlos Flores', email: 'cflores057@mail.fresnostate.edu', age: 26}
 http.createServer((req, res) => {
 res.writeHead(200, {'Content-Type': 'application\json'})
 res.write(JSON.stringify(data));
 res.end();
-}).listen(4000);
+}).listen(4000);*/
+
+const express = require('express'); 
+const app = express(); 
+const PORT = 4000; 
+
+app.use(express.json());
+
+const userData = require('./useraccount')
+
+app.get('/user', (req, res) => {
+    res.send(userData);
+})
+
+app.get('/user/:id', (req, res) => {
+    res.send(userData);
+})
+
+app.listen(PORT, ()=> {
+    console.log(`App is listening on poert ${PORT}`);
+})
